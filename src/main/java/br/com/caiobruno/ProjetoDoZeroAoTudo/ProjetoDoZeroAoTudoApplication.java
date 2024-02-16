a8707707b17891647d242e55f879878e49d475d2
CaioBrunoE <caiobruno90@yahoo.com>
2024-02-16 11:49:24 -0300

    Iniciando a api

---
+++ src/main/java/br/com/caiobruno/ProjetoDoZeroAoTudo/ProjetoDoZeroAoTudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.caiobruno.ProjetoDoZeroAoTudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDoZeroAoTudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDoZeroAoTudoApplication.class, args);
+	}
+
+}
